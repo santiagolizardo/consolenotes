e6c274472f26e078ffbc4493622334c8fd8ff344
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2019-12-25 19:10:54 +0000

    Remove warning about unused var

--- src/ui.c
+++ src/ui.c
@@ -5,6 +5,7 @@
 extern Dimension screen_size;
 
 void resize_handler(int sig) {
+	(void)sig; // Silent unused warning
 	getmaxyx(stdscr, screen_size.h, screen_size.w);
 	wnoutrefresh(stdscr);
 	doupdate();
